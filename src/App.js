da55a46d6ae572ad807e60aad11ede5a04669a19
Mukhtar Janagale <janogale@users.noreply.github.com>
2020-04-30 10:16:22 +0300

    alert added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 import "./app.css";
@@ -7,7 +7,17 @@ import Alert from "./components/Alert";
 
 function App() {
   const [students, setStudents] = useState([]);
-  const [alert, setAlert] = useState(true);
+  const [alert, setAlert] = useState({});
+  const [toggleAlert, setToggleAlert] = useState(false);
+
+  // useEffect - hide alert
+  useEffect(() => {
+    if (toggleAlert) {
+      setTimeout(() => {
+        setToggleAlert(false);
+      }, 2000);
+    }
+  }, [toggleAlert]);
 
   const removeStudent = (index) => {
     let filteredData = students.filter((student, i) => {
@@ -18,14 +28,29 @@ function App() {
   };
 
   const handleSubmit = (student) => {
-    setStudents([...students, student]);
+    if (student.name && student.course && student.mobile) {
+      setStudents([...students, student]);
+      setAlert({
+        message: "One Student Created.",
+        color: "#090",
+        bg: "#bef3be",
+      });
+      setToggleAlert(true);
+    } else {
+      setAlert({
+        message: "Please Fill all Fields",
+        color: "red",
+        bg: "#f3cad8",
+      });
+      setToggleAlert(true);
+    }
   };
 
   return (
     <>
       <Header />
       <div className="container">
-        <Alert />
+        {toggleAlert && <Alert alert={alert} />}
         <p>List of the Students</p>
         <div className="wrapper">
           <Table students={students} removeStudent={removeStudent} />
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export default function Alert() {
+export default function Alert({ alert = {} }) {
   return (
-    <div className="alert">
-      <h5>Alert</h5>
+    <div
+      className="alert"
+      style={{ background: alert.bg, borderLeft: `2px solid ${alert.color}` }}
+    >
+      <h5 style={{ color: alert.color }}>{alert.message}</h5>
     </div>
   );
 }
