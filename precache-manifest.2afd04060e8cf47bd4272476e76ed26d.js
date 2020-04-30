74df2d43f5d6f41f1f046ff174b631f8a88a5ed8
Mukhtar Janagale <janogale@users.noreply.github.com>
2020-04-30 10:20:12 +0300

    Updates

---
+++ precache-manifest.2afd04060e8cf47bd4272476e76ed26d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd090f0a69daeb9d4584",
+    "url": "/student-hub-react/static/css/main.cfb427e9.chunk.css"
+  },
+  {
+    "revision": "fd090f0a69daeb9d4584",
+    "url": "/student-hub-react/static/js/main.e1140677.chunk.js"
+  },
+  {
+    "revision": "eb652f79a4d0fb8cb830",
+    "url": "/student-hub-react/static/js/runtime~main.c86388f1.js"
+  },
+  {
+    "revision": "76d90b33bff5179c96ea",
+    "url": "/student-hub-react/static/js/2.066600f2.chunk.js"
+  },
+  {
+    "revision": "6f44899b874e025d3c4b04f4e49c008f",
+    "url": "/student-hub-react/index.html"
+  }
+];
